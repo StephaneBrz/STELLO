ebd8baf11e15c8cb2e33f45b664f1920734ce66d
Steph06600 <stephraynaud@gmail.com>
2022-06-30 09:41:37 +0200

    dernieres modifs

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,16 +4,17 @@
 
 <div class="container">
     <div class="row align-items-start">
-        <h2>{{ $project->name }} <a class="btn btn-warning" href="{{ route('projects.edit', $project->id) }}">Modifier le nom du projet</a></h2>
-            <button class="btn btn-danger" type="submit">Ajouter une catégorie</button>
-            <button class="btn btn-danger" type="submit">Ajouter une tâche</button>
-       <form action="{{ route('projects.destroy', $project->id) }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button class="btn btn-danger" type="submit">Supprimer le projet</button> 
-        </form>
+        <h2>{{ $project->name }}<a class="btn btn-warning" href="{{ route('projects.edit', $project->id) }}">Modifier le nom du projet</a>
+        <a href="{{ route('categories.edit', $project->id) }}"><button class="btn btn-danger" type="submit">Ajouter une catégorie</button></a>
+            <form action="{{ route('projects.destroy', $project->id) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger" type="submit">Supprimer le projet</button> 
+            </form>
+        </h2>
+            
+       
         <br>
-    <a href="{{ route('projects.index') }}" class="btn btn-secondary">Retour à la liste</a>
 
 
             <div class="container">
@@ -26,10 +27,10 @@
                                     </div>
                                     <div class="card-body">
                                         <ul>@foreach ($category->tasks as $task)
-                                            <button href="" class="btn btn-danger" type="submit">Modifier</button>
+                                            
                                             <li class="card-text">
                                                 {{$task->name}}
-                                                
+                                                <button href="" class="btn btn-danger" type="submit">Modifier</button>
                                                 <button class="btn btn-danger" type="submit">Ajouter une tâche</button>
                                                 <button class="btn btn-danger" type="submit">Supprimer la tâche></label></button>
                                             </li> 
